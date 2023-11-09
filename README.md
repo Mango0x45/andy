d1854be56db66182b380779c2c94a62f4de01d7a
Thomas Voss <mail@thomasvoss.com>
2023-11-09 13:14:37 +0100

    Add the ‘cd’ builtin

--- README.md
+++ README.md
@@ -22,6 +22,7 @@ a while.  The currently implemented features are:
 - [X] Write to /dev/null (`>_`)
 - [X] Pipelines (`cmd1 | … | cmdN`)
 - [X] Condition chains (`cmd1 && … || cmdN`)
+- [X] `cd` builtin function
 - [X] `echo` builtin function
 - [X] `true` builtin function
 - [X] `false` builtin function
--- vm/builtins.go
+++ vm/builtins.go
@@ -2,15 +2,40 @@ package vm
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"os/user"
 )
 
 type builtin func(cmd *exec.Cmd) commandResult
 
 var builtins = map[string]builtin{
+	"cd":    builtinCd,
 	"echo":  builtinEcho,
 	"false": builtinFalse,
 	"true":  builtinTrue,
+}
+
+func builtinCd(cmd *exec.Cmd) commandResult {
+	var dst string
+	switch len(cmd.Args) {
+	case 1:
+		user, err := user.Current()
+		if err != nil {
+			return errInternal{err}
+		}
+		dst = user.HomeDir
+	case 2:
+		dst = cmd.Args[1]
+	default:
+		fmt.Fprintln(cmd.Stderr, "Usage: cd [directory]")
+		return errExitCode(1)
+	}
+
+	if err := os.Chdir(dst); err != nil {
+		return errInternal{err}
+	}
+	return errExitCode(0)
 }
 
 func builtinEcho(cmd *exec.Cmd) commandResult {
