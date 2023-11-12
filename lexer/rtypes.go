1c2500c112ef797d1401050c907123f09ccdc15c
Thomas Voss <mail@thomasvoss.com>
2023-11-12 12:09:52 +0100

    Add quotation marks as metachars

--- lexer/rtypes.go
+++ lexer/rtypes.go
@@ -1,7 +1,12 @@
 package lexer
 
 func isMetachar(r rune) bool {
-	return r == '|' || r == '>' || r == '<' || r == '&'
+	return r == '\'' ||
+		r == '"' ||
+		r == '|' ||
+		r == '>' ||
+		r == '<' ||
+		r == '&'
 }
 
 func isEol(r rune) bool {
