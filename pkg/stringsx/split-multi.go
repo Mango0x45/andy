fad01b0d3c1f25969c57943d9cf1a790a17708d5
Thomas Voss <mail@thomasvoss.com>
2023-12-05 19:19:23 +0100

    Code style

--- pkg/stringsx/split-multi.go
+++ pkg/stringsx/split-multi.go
@@ -6,7 +6,6 @@ func SplitMulti(s string, seps []string) []string {
 	out := make([]string, 0, 8)
 
 	var i int
-
 	for j := 0; j < len(s); j++ {
 		for _, sep := range seps {
 			if !strings.HasPrefix(s[j:], sep) {
