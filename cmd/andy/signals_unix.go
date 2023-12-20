53c809eef30403284256b86960e69a11454485a0
Thomas Voss <thomas.voss@humanwave.nl>
2023-12-20 13:58:30 +0100

    Fix shit

---
+++ cmd/andy/signals_unix.go
@@ -0,0 +1,34 @@
+//go:build unix && !(linux || darwin)
+package main
+
+import (
+	"os"
+	"syscall"
+)
+
+var signals = map[string]os.Signal{
+	"sigabrt": syscall.SIGABRT,
+	"sigalrm": syscall.SIGALRM,
+	"sigbus":  syscall.SIGBUS,
+	"sigchld": syscall.SIGCHLD,
+	"sigcont": syscall.SIGCONT,
+	"sigfpe":  syscall.SIGFPE,
+	"sighup":  syscall.SIGHUP,
+	"sigill":  syscall.SIGILL,
+	"sigint":  syscall.SIGINT,
+	"sigkill": syscall.SIGKILL,
+	"sigpipe": syscall.SIGPIPE,
+	"sigquit": syscall.SIGQUIT,
+	"sigsegv": syscall.SIGSEGV,
+	"sigstop": syscall.SIGSTOP,
+	"sigterm": syscall.SIGTERM,
+	"sigtrap": syscall.SIGTRAP,
+	"sigtstp": syscall.SIGTSTP,
+	"sigttin": syscall.SIGTTIN,
+	"sigttou": syscall.SIGTTOU,
+	"sigurg":  syscall.SIGURG,
+	"sigusr1": syscall.SIGUSR1,
+	"sigusr2": syscall.SIGUSR2,
+	"sigxcpu": syscall.SIGXCPU,
+	"sigxfsz": syscall.SIGXFSZ,
+}
